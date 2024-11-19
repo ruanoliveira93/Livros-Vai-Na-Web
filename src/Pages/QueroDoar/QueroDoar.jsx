69c3d7c44ca27c47e6e26adc10f1cd2172175041
Ruan Oliveira <ruanwesley93@gmail.com>
2024-11-18 23:40:16 -0300

    new commit

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -18,7 +18,7 @@ export default function queroDoar() {
                     <input type="text" name="categoria" id="categoria" placeholder="Categoria" />
                     <input type="text" name="autor" id="autor" placeholder="Autor" />
                     <input type="url" name="linkImagem" id="linkImagem" placeholder="Link da Imagem" />
-                    <input type="submit" id="btn" value="Doar" />
+                    <input type="submit" value="Doar" />
                 </form>
 
             </section>
