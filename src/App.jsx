8adb1d95767924ee9aac33c6f4f17d8090c51ecb
Ruan Oliveira <ruanwesley93@gmail.com>
2024-11-21 17:32:28 -0300

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
-
+import './GlobalStyle/globalStyle.scss'
 export default function App() {
   return (
      <>
